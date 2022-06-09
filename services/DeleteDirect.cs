ecde354ed3ea789320d30d9f8eb0451797deb65f
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-06-09 16:30:49 +0700

    solution for deleteWord

--- services/DeleteDirect.cs
+++ services/DeleteDirect.cs
@@ -7,7 +7,10 @@ namespace DeleteDic
         public string Wordname;
         public string SearchDirect(Wordname)
         {
-            Console.WriteLine("Nhập vào đây từ bạn muốn tìm: ");
+            Console.WriteLine("Nhập vào đây từ bạn muốn xóa: ");
+            // Highest priority: viết method xóa từ
+            // B1: Dùng vòng lặp for/câu điều kiện if else để tìm xem từ đó có tồn tại trong list hay không
+            // B2: Nếu tìm được từ thì hỏi ng dùng có muốn xóa hay không (Y/N) (sử dụng switch case hoặc if else)
             Wordname = Console.ReadLine();
         }
     }
