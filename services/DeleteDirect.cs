c045ecff7b1e2a52d0f196802292d6ed15ee0789
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-06-09 16:26:19 +0700

    addDictionaries

---
+++ services/DeleteDirect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DeleteDic
+{
+    class DeleteDirect
+    {
+        public string Wordname;
+        public string SearchDirect(Wordname)
+        {
+            Console.WriteLine("Nhập vào đây từ bạn muốn tìm: ");
+            Wordname = Console.ReadLine();
+        }
+    }
+}
